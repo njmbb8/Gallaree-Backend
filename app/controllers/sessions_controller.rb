acb7247e80c65972e6f6306521aa8c19b6257f6c
njmbb8 <nmcgr001@gmail.com>
2022-01-21 16:16:26 -0500

    User registration works, still need validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
-end
+    def create
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    # rescue_from ActiveRecord::RecordInvalid with: :return_unprocessable
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :firstname, :lastname, :addr1, :addr2, :city, :state, :zip, :password, :password_confirmation)
+    end
+
+    # def return_unprocessable(error)
+    #     render json: 
+    # end
 end
