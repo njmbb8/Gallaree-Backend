eb1d45aac668e1fdef2f1537598b9abd66e04e15
njmbb8 <nmcgr001@gmail.com>
2022-02-23 20:45:47 -0500

    configure devise for email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # default url for devise
+  # devise configurations
   config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 end
