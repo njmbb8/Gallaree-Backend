ad6e63f6d0ecbb1f7422917878bc39a98d9e47b3
njmbb8 <nmcgr001@gmail.com>
2022-03-06 19:28:18 -0500

    Update controller to save new address with new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
-    # rescue_from ActiveRecord::RecordInvalid with: :return_unprocessable
     def create
-        user = User.create(user_params)
-        if user.valid?
-            user.send_confirmation_email!
+        @user = User.new(user_params)
+        if @user.save
+            @user.send_confirmation_email!
             cookies.permanent[:user_id] = {
-                value: user.id,
+                value: @user.id,
                 domain: :all
             }
-            render json: user, status: :created
+            render json: @user, status: :created
         else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
     def show
-        user = User.find(cookies[:user_id])
-        if user
-            render json: user
+        @user = User.find(cookies[:user_id])
+        if @user
+            render json: @user
         else
             render json: { errors: "Not Authorized" }
         end
@@ -26,13 +25,9 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(
-            :email, :firstname, :lastname, :password, :password_confirmation, :admin,
-            addresses_attributes: [:address_line1, :address_line2, :city, :state, :postal_code, :country]
+        params.require(:user).permit(
+            :email, :firstname, :lastname, :password, :password_confirmation,
+            addresses_attributes: [:id, :address_line1, :address_line2, :city, :state, :postal_code, :country]
         )
     end
-
-    # def return_unprocessable(error)
-    #     render json: 
-    # end
 end
