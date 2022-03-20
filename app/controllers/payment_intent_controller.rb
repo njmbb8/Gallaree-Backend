e80d567775f8d7874ea5e4df82f30979d9b27de0
njmbb8 <nmcgr001@gmail.com>
2022-03-19 20:29:07 -0400

    add paymentintent create action and route

--- app/controllers/payment_intent_controller.rb
+++ app/controllers/payment_intent_controller.rb
@@ -1,5 +1,11 @@
 class PaymentIntentController < ApplicationController
     def create
-        
+        @payment_intent = Stripe::PaymentIntent.create(
+            amount: Order.find(params[:order_id]).order_items.sum { |item| item.art.price * item.quantity },
+            currency: 'usd',
+            automatic_payment_methods: {
+                enabled: true
+            }
+        )
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :payment_intents, only: [:create]
   resources :order_items, only: [:create, :destroy, :update]
   resources :order, only: [:show]
+  resources :payment_intents, only: [:create]
   post '/register', to: 'users#create'
   post '/login', to: 'sessions#create'
   get '/me', to: 'users#show'
