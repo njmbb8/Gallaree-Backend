5f0a4acaaaaff5e72dd842395a350a2c2cb4a7fd
njmbb8 <nmcgr001@gmail.com>
2022-03-24 16:37:18 -0400

    add flags for default shipping and billing on addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_003204) do
+ActiveRecord::Schema.define(version: 2022_03_24_183833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2022_03_22_003204) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "state"
+    t.boolean "shipping"
+    t.boolean "billing"
   end
 
   create_table "arts", force: :cascade do |t|
