d1f03d6d72f9b339b9de77b87ae8c905ff4a33a5
njmbb8 <nmcgr001@gmail.com>
2022-03-25 20:20:54 -0400

    output for update and delete

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,6 @@
 class AddressesController < ApplicationController
     before_action :check_ownership, only: [:update, :destroy]
-
+    
     def create
         @user = User.find(cookies[:user_id])
         if @user
@@ -14,28 +14,32 @@ class AddressesController < ApplicationController
             render json: { error: "could not find user" }, status: :not_found
         end
     end
-
+    
     def update
-        @address = address_params
-        @address.save
+        if @address.update(address_params)
+            render json: @address, status: :ok
+        else
+            render json: { error: "could not update address"}, status: :unprocessable_entity
+        end
     end
 
     def destroy
         @address.destroy
+        head :ok
     end
 
     private
 
     def check_ownership
-        @address = User.find(params[:id])
+        @address = Address.find(params[:id])
         if @address
-            render json: {error: "address does not belon got user"}, status: :unauthorized unless @address.user.id == cookies[:user_id].to_i
+            render json: {error: "address does not belong to user"}, status: :unauthorized unless @address.user.id == cookies[:user_id].to_i
         else
             render json: {error: "address not found"}, status: :not_found
         end
     end
 
     def address_params
-        params.permit(:address_line1, :address_line2, :city, :postal_code, :country, :state)
+        params.permit(:address_line1, :address_line2, :city, :postal_code, :country, :state, :shipping, :billing)
     end
 end
