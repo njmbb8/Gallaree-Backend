c47840b704952bd3f9587a9d67d0db8c83168e5d
njmbb8 <nmcgr001@gmail.com>
2022-03-31 18:36:47 -0400

    update strong params

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -28,6 +28,6 @@ class OrderController < ApplicationController
     private
 
     def order_params
-        params.permit(:user_id, :order_status, :tracking, :shipping_id, :payment_intent)
+        params.permit(:user_id, :order_status_id, :tracking, :shipping_id, :payment_intent)
     end
 end
