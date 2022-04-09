eff59c14d01997fefd0d2e6b4946076f705bcd54
njmbb8 <nmcgr001@gmail.com>
2022-04-09 09:35:23 -0400

    Simplify cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
-            cookies.permanent.signed[:user_id] = {
-                value: user.id,
-                domain: :all
-            }
+            cookies.permanent.signed[:user_id] = user.id
             render json: user, status: :created
         else
             render json: { error: "Invalid username or password" }, status: :unauthorized
