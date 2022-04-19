65a9d164aabbc2a7b06787f62c819a005eca23d1
njmbb8 <nmcgr001@gmail.com>
2022-04-18 20:42:07 -0400

    include fee information in order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :order_total, :tracking, :payment_intent, :address, :shipping_id
+  attributes :id, :order_total, :tracking, :payment_intent, :address, :shipping_id, :stripe_fee, :total_with_fee
 
   has_many :order_items
   belongs_to :user
@@ -7,6 +7,14 @@ class OrderSerializer < ActiveModel::Serializer
 
   def order_total
     object.order_items.sum { |item| item.art.price * item.quantity }
+  end
+
+  def stripe_fee
+    (order_total * 0.029) + 0.30
+  end
+
+  def total_with_fee
+    order_total + stripe_fee
   end
 
   def address
