9f806d929d3f13180af88ca97fa789e897aa4323
njmbb8 <nmcgr001@gmail.com>
2022-10-18 22:13:27 -0400

    Address updates now work with Stripe API

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,62 +1,17 @@
-class AddressesController < ApplicationController
-    before_action :check_ownership, only: [:update, :destroy]
-    
+class AddressesController < ApplicationController    
     def create
-        @user = User.find(cookies.signed[:user_id])
-        if @user
-            @address = @user.addresses.new(address_params)
-            if @address.shipping || @user.addresses.empty?
-                set_default
-            end
-            if @address.save
-                render json: @address, status: :ok
-            else
-                render json: { error: "Could not save address" }, status: :unprocessable_entity
-            end
+        user = User.find(cookies.signed[:user_id])
+        if user
+            Stripe::Customer.update(user.stripe_id, address_params.to_h)
+            render json: user, status: :created
         else
-            render json: { error: "could not find user" }, status: :not_found
+            render json: {error: 'You are not logged in'}, status: :unauthorized
         end
-    end
-    
-    def update
-        if @address.shipping == false && address_params[:shipping] == "true"
-            set_default
-        end
-        if @address.update(address_params)
-            render json: @address, status: :ok
-        else
-            render json: { error: "could not update address"}, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @address.update(archived: true)
-        if @address.user.orders.last.shipping_id == @address.id
-            @address.user.orders.last.update(shipping_id: @address.user.addresses.find_by(shipping: true).id)
-        end
-        if @address.shipping
-            @address.user.addresses.where(:archived => false).first.update(shipping: true)
-        end
-        head :ok
     end
 
     private
-    def set_default
-        @address.user.addresses.map {|address| address.update(shipping: false)}
-        @address.update(shipping: true)
-    end
-
-    def check_ownership
-        @address = Address.find(params[:id])
-        if @address
-            render json: {error: "address does not belong to user"}, status: :unauthorized unless @address.user.id == cookies.signed[:user_id].to_i
-        else
-            render json: {error: "address not found"}, status: :not_found
-        end
-    end
 
     def address_params
-        params[:postal_code] = params[:postal_code].to_i
-        params.permit(:address_line1, :address_line2, :city, :postal_code, :country, :state, :shipping, :billing)
+        params.require(:addresses).permit(:address => [:city, :country, :line1, :line2, :postal_code, :state], :shipping => [:name, :phone, :address => [:city, :country, :line1, :line2, :postal_code, :state]])
     end
 end
