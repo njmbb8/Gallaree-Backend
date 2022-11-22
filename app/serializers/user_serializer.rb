d4593cec33d65c88226fa6676f1e380438ec470f
njmbb8 <nmcgr001@gmail.com>
2022-11-21 20:16:26 -0500

    Update user serializer to show billing and shipping addresses

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :stripe_info, :active_order, :admin
+  attributes :id, :stripe_info, :active_order, :admin, :addresses
 
-  has_many :addresses
+  def addresses
+    {
+      shipping: object.addresses.find_by(shipping: true),
+      billing: object.addresses.find_by(billing: true)
+    }
+  end
 
   def active_order
     OrderSerializer.new(object.orders.last, each_serializer: OrderSerializer)
