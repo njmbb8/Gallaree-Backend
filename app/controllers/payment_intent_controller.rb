130c2a7cd9db6a51b3e3bd667417c8f480f501dc
njmbb8 <nmcgr001@gmail.com>
2022-11-26 17:21:32 -0500

    update payment intent creation

--- app/controllers/payment_intent_controller.rb
+++ app/controllers/payment_intent_controller.rb
@@ -1,29 +1,34 @@
 class PaymentIntentController < ApplicationController
     def create
         user = User.find(cookies.signed[:user_id])
-        shipping_address = user.addresses.find_by(shipping: true)
+        shipping_address = user.addresses.find(intent_params[:shipping_id])
         @order = user.orders.last
         @payment_intent = Stripe::PaymentIntent.create(
             amount: calculate_total,
             currency: 'usd',
-            automatic_payment_methods: {
-                enabled: true
+            automatic_payment_methods:{
+                enabled: false
             },
-            receipt_email: user.email,
+            confirm: true,
+            customer: user[:stripe_id],
+            off_session: false,
+            payment_mathod: intent_params[:payment_method],
+            payment_method_types: ['card'],
+            receipt_email: user[:email],
+            setup_future_usage: 'on_session',
             shipping: {
+                name: "#{user.firstname} #{user.lastname}",
                 address: {
-                    city: shipping_address.city,
-                    country: shipping_address.country,
-                    line1: shipping_address.address_line1,
-                    line2: shipping_address.address_line2,
-                    postal_code: shipping_address.postal_code,
-                    state: shipping_address.state
-                },
-                name: "#{user.firstname} #{user.lastname}"
-            },
-            statement_descriptor: "Order #: #{user.orders.last.id}"
+                    line1: shipping_address[:line1],
+                    line2: shipping_address[:line2],
+                    city: shipping_address[:city],
+                    state: shipping_address[:state],
+                    postal_code: shipping_address[:postal_code]
+                }
+            }
+
         )
-        render json: { clientSecret: @payment_intent['client_secret'], payment_intent: @payment_intent[:id]}, status: :ok
+        render json: { id: payment_intent[:id], client_secret: @payment_intent[:client_secret]}, status: :ok
     end
 
     def update
@@ -52,7 +57,6 @@ class PaymentIntentController < ApplicationController
         else
             render json: {error: "order not found"}, status: :not_found
         end
-
     end
 
     private
@@ -61,5 +65,12 @@ class PaymentIntentController < ApplicationController
         sum = @order.order_items.sum {|item| item.art.price * item.quantity} * 100
         sum_with_percentage = sum + (0.029 * sum)
         (sum_with_percentage + 30).ceil
+    end
+
+    def intent_params
+        params.permit(
+            :payment_method_id,
+            :shipping_id
+        )
     end
 end
