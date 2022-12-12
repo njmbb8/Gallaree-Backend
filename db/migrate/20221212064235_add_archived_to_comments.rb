ddd3088f41f58d77faae113750983ff258ad738b
njmbb8 <nmcgr001@gmail.com>
2022-12-12 01:43:34 -0500

    add archived flag to comment

---
+++ db/migrate/20221212064235_add_archived_to_comments.rb
@@ -0,0 +1,5 @@
+class AddArchivedToComments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :comments, :archived, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_051536) do
+ActiveRecord::Schema.define(version: 2022_12_12_064235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2022_12_12_051536) do
     t.integer "blog_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "archived"
   end
 
   create_table "order_items", force: :cascade do |t|
