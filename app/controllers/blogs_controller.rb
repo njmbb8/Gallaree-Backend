e58dcba320b886d8a6732a93e865e94bcedda672
njmbb8 <nmcgr001@gmail.com>
2022-12-12 01:17:39 -0500

    create controller for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,16 +1,54 @@
 class BlogsController < ApplicationController
+
+  before_action :authorize, except: [:show, :index]
+  before_action :find_blog, except: [:index, :create]
+
   def index
+    render json: Blog.all, status: :ok
   end
 
   def show
+    render json: @blog, status: :ok
   end
 
   def create
+    blog = Blog.new(blog_params)
+    if blog.save
+      render json: blog, status: :ok
+    else
+      render json: {error: "could not save blog post"}, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @blog.update(blog_params)
+      render json: @blog, status: :ok
+    else
+      render json: {error: "unable to update blog"}, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @blog.destroy
+      head :ok
+    else
+      render json: {error: "could not delete post"}, status: :unprocessable_entity
+    end
   end
+end
+
+private
+
+def authorize
+  user = User.find(cookies.signed[:user_id])
+  render json: {error: "you are not signed in or do not have permission to edit blogs"}, status: :forbidden if user.nil? || !user.admin
+end
+
+def find_blog
+  @blog = Blog.find(params[:id])
+  render json: {error: "unable to locate blog"}, status: :not_found if @blog.nil?
+end
+
+def blog_params
+  params.permit(:title, :body)
 end
