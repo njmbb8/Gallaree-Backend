8f3457b2ef849d37bcba715a4f85048e09f6dcd2
njmbb8 <nmcgr001@gmail.com>
2022-12-12 02:11:04 -0500

    create serializer for blogs

---
+++ app/serializers/blog_serializer.rb
@@ -0,0 +1,12 @@
+class BlogSerializer < ActiveModel::Serializer
+    include Rails.application.routes.url_helpers
+
+    has_many :comments
+    belongs_to :user
+
+    attributes :title, :body, :created_at, :updated_at
+
+    def photo
+        rails_blob_path(object.photo, only_path: true)
+    end
+end
