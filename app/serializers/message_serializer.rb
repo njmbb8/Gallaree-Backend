04f1d23bf72e0b9223fba729ca2139c9f1e81361
njmbb8 <nmcgr001@gmail.com>
2022-12-22 21:55:25 -0500

    update message serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -2,12 +2,21 @@ class MessageSerializer < ActiveModel::Serializer
     attributes :body, :created_at, :read, :sender
 
     def sender
-        user = User.find(object.user_id)
-
-        return {
-            :id => user[:id],
-            :name => "#{user[:firstname]} #{user[:lastname]}",
-            :email => user[:email]
-        }
+        if !!object.user_id
+            user = User.find(object.user_id)
+            return {
+                :id => user[:id],
+                :name => "#{user[:firstname]} #{user[:lastname]}",
+                :phone => user[:phone],
+                :email => user[:email]
+            }
+        else
+            return {
+                :id => 0,
+                :name => object.conversation.recipient_name,
+                :phone => object.conversation.recipient_phone,
+                :email => object.conversation.recipient_email
+            }
+        end
     end
 end
