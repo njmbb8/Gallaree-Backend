b35aab06fe83bb3006f9865c101f442fc3afcba0
njmbb8 <nmcgr001@gmail.com>
2022-12-26 18:52:57 -0500

    send url to image with blog

--- app/serializers/blog_list_serializer.rb
+++ app/serializers/blog_list_serializer.rb
@@ -1,13 +1,13 @@
 class BlogListSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :id, :title, :text_preview, :created_at, :updated_at
+    attributes :id, :photo, :title, :text_preview, :created_at, :updated_at
 
     def photo
         rails_blob_path(object.photo, only_path: true)
     end
 
     def text_preview
-        truncate(object.body, 50)
+        object.body.truncate(50)
     end
 end
