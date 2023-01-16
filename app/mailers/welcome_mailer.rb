d5907571d44ba8f8399f6b0d1e7ac4f18cc01db5
njmbb8 <nmcgr001@gmail.com>
2023-01-16 14:39:15 -0500

    remove expiration from account confirmation token

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,7 +1,7 @@
 class WelcomeMailer < ApplicationMailer
     def welcome
         @user = params[:user]
-        @confirmation_token = params[:user].signed_id(purpose: 'account_confirmation', expires_in: 15.minutes)
+        @confirmation_token = params[:user].signed_id(purpose: 'account_confirmation')
     
         mail to: @user.email, subject: "Welcome!"
     end
